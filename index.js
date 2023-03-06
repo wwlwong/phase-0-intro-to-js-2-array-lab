802e9819f0b4a2a0f8095f44373e74a568622a70
William Wong <william.wai.lim.wong@gmail.com>
2023-03-06 15:55:00 -0500

    Completed Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name){
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name){
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat(){
+    const copyOfCats = [...cats];
+    copyOfCats.splice(-1);
+    return copyOfCats;
+}
+
+function removeFirstCat(){
+    const copyOfCats = [...cats];
+    copyOfCats.splice(0,1);
+    return copyOfCats;
+}
